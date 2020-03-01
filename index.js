d25011579f18439c4a3edfb660752ed813ea783c
andrewhlu <alu@ucsb.edu>
2020-02-29 18:05:50 -0800

    Add Firebase

---
+++ index.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBl2ghVkvo9skQwo8jkH58Mwzc7bOK8Moo",
+    authDomain: "sbhacks-b2019.firebaseapp.com",
+    databaseURL: "https://sbhacks-b2019.firebaseio.com",
+    projectId: "sbhacks-b2019",
+    storageBucket: "sbhacks-b2019.appspot.com",
+    messagingSenderId: "937961984218",
+    appId: "1:937961984218:web:6b7fd909cd26eb868755e3",
+    measurementId: "G-7YHG475TJB"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
